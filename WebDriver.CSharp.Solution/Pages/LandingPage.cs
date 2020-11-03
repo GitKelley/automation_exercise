f7941c235fa7fecbdd63d121d12f1c82f7bbc902
John Kelley <johncharleskelley@gmail.com>
2020-11-02 20:03:38 -0500

    first commit

---
+++ WebDriver.CSharp.Solution/Pages/LandingPage.cs
@@ -0,0 +1,98 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace WebDriver.CSharp.Solution.Pages
+{
+    class LandingPage
+    {
+
+        [FindsBy(How = How.Id, Using = "run-button")]
+        private IWebElement runButton;
+
+        [FindsBy(How = How.Id, Using = "output")]
+        private IWebElement outputBox;
+
+        [FindsBy(How = How.Id, Using = "login-modal-label")]
+        private IWebElement logInModalHeader;
+
+        [FindsBy(How = How.CssSelector, Using = ".nuget-panel > input")]
+        private IWebElement nuGetInput;
+
+        [FindsBy(How = How.ClassName, Using = ".package-name")]
+        private IWebElement nuGetPackageList;
+
+        [FindsBy(How = How.Id, Using = "Share")]
+        private IWebElement shareButton;
+
+        [FindsBy(How = How.Id, Using = "ShareLink")]
+        private IWebElement shareText;
+
+        [FindsBy(How = How.ClassName, Using = "glyphicon-chevron-left")]
+        private IWebElement closeOptionsButton;
+
+        [FindsBy(How = How.Id, Using = "save-button")]
+        private IWebElement saveButton;
+
+        [FindsBy(How = How.LinkText, Using = "/GettingStarted/")]
+        private IWebElement gettingStartedButton;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='top - navbar']/*/span[contains(text(),'Back To Editor')]")]
+        private IWebElement backToEditorButton;
+
+        public void run()
+        {
+            runButton.Click();
+        }
+
+        public string verifyOutputText()
+        {
+            string output = outputBox.Text;
+            return output;
+        }
+
+        public void selectNuGetPackage()
+        {
+            nuGetInput.SendKeys("nUnit");
+            nuGetInput.FindElement(By.XPath("//*[@id='menu']/li/a/span[contains(text(), 'NUnit')]")).Click();
+            nuGetInput.FindElement(By.XPath("//*[@id='menu']/li[1]/ul/li[1]/a")).Click();
+        }
+
+        public string getNuGetPackage()
+        {
+            string packageName = nuGetPackageList.Text;
+            return packageName;
+        }
+
+        public void share()
+        {
+            shareButton.Click();
+        }
+
+        public string getShareLink()
+        {
+            string linkText = shareText.GetAttribute("value");
+            return linkText;
+        }
+
+        public void closeOptions()
+        {
+            closeOptionsButton.Click();
+        }
+
+        public void save()
+        {
+            saveButton.Click();
+        }
+
+        public void getStarted()
+        {
+            gettingStartedButton.Click();
+        }
+
+        public void backToEditor()
+        {
+            backToEditorButton.Click();
+        }
+
+    }
+}
---
+++ WebDriver.CSharp.Solution/Spec/Solution_Spec.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumExtras.PageObjects;
+using WebDriver.CSharp.Solution.Pages;
+
+namespace WebDriver.CSharp.Solution.Spec
+{
+    class Solution_Spec
+    {
+        private IWebDriver driver;
+        public string shareLinkText;
+        public string testUrl;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            testUrl = "https://dotnetfiddle.net/";
+            driver.Navigate().GoToUrl(testUrl);
+        }
+
+        /* Share link text value is never actually accessible via the DOM. 
+         * Getting the value attribute retrieves a null value, causing the below assert to fail.
+         * I've still written an example of an assertion that should return true, given the value is actually populated
+        */
+        [Test(Description = "Click Share button, verify the link in the field begins with the correct text")]
+        public void verifyShareFunctionality()
+        {
+            LandingPage landingPage = new LandingPage();
+            PageFactory.InitElements(driver, landingPage);
+            landingPage.share();
+            //shareLinkText = landingPage.getShareLink();
+            //Assert.IsTrue(shareLinkText.Contains("https://dotnetfiddle.net/"), "link did not begin with expected text");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+
+    }
+}
